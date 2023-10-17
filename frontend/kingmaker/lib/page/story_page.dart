a12efec77f452f14478c20038564846855eafe47
JHT <jht043@naver.com>
2023-10-17 23:51:24 +0900

    :zap: feat: 스토리페이지타이핑효과발견

--- frontend/kingmaker/lib/page/story_page.dart
+++ frontend/kingmaker/lib/page/story_page.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class StoryPage extends StatelessWidget {
   const StoryPage({super.key});
@@ -18,37 +19,26 @@ class StoryPage extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Image(image: AssetImage(img), height: 300),
-          AutoSizeText(
-            storyList[0],
-            style: const TextStyle(
-              fontSize: 20,
-              color: Colors.white,
+          SizedBox(
+            height: 200,
+            child: AnimatedTextKit(
+              animatedTexts: [
+                TypewriterAnimatedText(
+                  '약속을 까먹고 급하게 뛰어가던 000\n핸들이 고장난 8톤 트럭에 부딫히고 마는데...\n눈을 뜬 000 앞에 등장한 여신...\n“이세계에 가서 규칙적인 삶을 산다면 너는 왕이 될 것이다!"',
+                  textStyle: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                  speed: const Duration(milliseconds: 150),
+                ),
+              ],
+              repeatForever: false,
+              isRepeatingAnimation: false,
+              pause: const Duration(),
+              displayFullTextOnTap: true,
+              stopPauseOnTap: false,
             ),
-            maxLines: 1,
-          ),
-          AutoSizeText(
-            storyList[1],
-            style: const TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-            ),
-            maxLines: 2,
-          ),
-          AutoSizeText(
-            storyList[2],
-            style: const TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-            ),
-            maxLines: 2,
-          ),
-          AutoSizeText(
-            storyList[3],
-            style: const TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-            ),
-            maxLines: 2,
           ),
         ],
       ),
