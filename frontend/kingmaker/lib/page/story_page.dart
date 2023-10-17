3cb8cb9e670f2d4b076dc9e0a0978587dd04f0d3
JHT <jht043@naver.com>
2023-10-18 00:45:16 +0900

    :zap: feat: 텍스트에니메이션 테스트

--- frontend/kingmaker/lib/page/story_page.dart
+++ frontend/kingmaker/lib/page/story_page.dart
@@ -1,7 +1,6 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:kingmaker/widget/common/bottom_nav_bar.dart';
 
 class StoryPage extends StatelessWidget {
   const StoryPage({super.key});
@@ -9,38 +8,52 @@ class StoryPage extends StatelessWidget {
   Widget build(BuildContext context) {
     String img = "assets/signup/story1.png";
     List<String> storyList = [
-      "약속을 까먹고 급하게 뛰어가던 000 ",
-      "핸들이 고장난 8톤 트럭에 부딫히고 마는데...",
-      "눈을 뜬 000 앞에 등장한 여신...",
-      '“이세계에 가서 규칙적인 삶을 산다면 너는 왕이 될 것이다!"'];
+      "약속장소로 급하게 뛰어가던 당신 ",
+      "핸들이 고장난 8t 트럭에 부딪히는데...",
+      "눈을 뜬 당신 앞에 등장한 여신...",
+      '“이세계에 가서 규칙적인 삶을 산다면..."',
+      '"당신은 왕이 될 것이다!"'];
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Image(image: AssetImage(img), height: 300),
-          SizedBox(
-            height: 200,
-            child: AnimatedTextKit(
-              animatedTexts: [
-                TypewriterAnimatedText(
-                  '약속을 까먹고 급하게 뛰어가던 000\n핸들이 고장난 8톤 트럭에 부딫히고 마는데...\n눈을 뜬 000 앞에 등장한 여신...\n“이세계에 가서 규칙적인 삶을 산다면 너는 왕이 될 것이다!"',
-                  textStyle: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+      body:GestureDetector(
+        onTap: (){
+          Navigator.pushAndRemoveUntil(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => BottomNavBar()), (route) => false
+          );
+        },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image(image: AssetImage(img), height: 300),
+            const SizedBox(height: 200),
+            SizedBox(
+              height: 100,
+              child: AnimatedTextKit(
+                animatedTexts: [
+                  ...List.generate(storyList.length, (index) =>
+                      TypewriterAnimatedText(
+                        storyList[index],
+                        textAlign: TextAlign.center,
+                        textStyle: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                        speed: const Duration(milliseconds: 150),
+                      ),
                   ),
-                  speed: const Duration(milliseconds: 150),
-                ),
-              ],
-              repeatForever: false,
-              isRepeatingAnimation: false,
-              pause: const Duration(),
-              displayFullTextOnTap: true,
-              stopPauseOnTap: false,
+                ],
+                repeatForever: false,
+                isRepeatingAnimation: false,
+                displayFullTextOnTap: true,
+                stopPauseOnTap: false,
+                pause: const Duration(milliseconds: 0),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
