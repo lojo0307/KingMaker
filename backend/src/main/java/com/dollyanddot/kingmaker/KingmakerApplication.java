754eb78732b98d69bcd48d4f85a45b7e93d7234c
허성백 <heosb123@hanmail.net>
2023-10-18 14:36:26 +0900

    :tada:init: backend project initialization

---
+++ backend/src/main/java/com/dollyanddot/kingmaker/KingmakerApplication.java
@@ -0,0 +1,13 @@
+package com.dollyanddot.kingmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingmakerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KingmakerApplication.class, args);
+  }
+
+}
