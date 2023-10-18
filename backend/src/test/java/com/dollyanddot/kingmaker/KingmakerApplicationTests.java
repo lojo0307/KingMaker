fd792da179867144cb2e56fffd420f67e07beb61
허성백 <heosb123@hanmail.net>
2023-10-18 14:36:26 +0900

    :tada:init: backend project initialization

---
+++ backend/src/test/java/com/dollyanddot/kingmaker/KingmakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dollyanddot.kingmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KingmakerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
