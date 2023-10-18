5f7d7678d97070e635a4a5d254c472f936d90b62
JHT <jht043@naver.com>
2023-10-19 01:09:43 +0900

    :sparkles: feat: 회원가입페이지 개발

--- frontend/kingmaker/lib/page/signup_page.dart
+++ frontend/kingmaker/lib/page/signup_page.dart
@@ -1,27 +1,80 @@
 import 'package:flutter/material.dart';
 import 'package:kingmaker/widget/common/bottom_nav_bar.dart';
-class SignupPage extends StatelessWidget {
+import 'package:kingmaker/widget/profile/profile_char_image_widget.dart';
+class SignupPage extends StatefulWidget {
   const SignupPage({super.key});
 
   @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  String error = "이름은 10자 이하로 지어주십시오.";
+  String name ="";
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          fit: BoxFit.cover,
-          image: AssetImage('assets/login/login.png'), // 배경 이미지
-        ),
-      ),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: GestureDetector(
-          onTap: (){
-            Navigator.pushAndRemoveUntil(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => const BottomNavBar()), (route) => false
-            );
-          },
+    return Scaffold(
+      backgroundColor: const Color(0xFFEDF1FF),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const ProfileCharImageWidget(),
+            const Text('Your majesty,',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Text('당신의 존함을 알려주십시오.',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child:Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Flexible(
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: TextField(
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                          ),
+                          onChanged: (value) {
+                            name = value;
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                  TextButton(onPressed: () {
+                    print(name);
+                  }, child: const Text("랜덤 생성")),
+                ],
+              ),
+            ),
+            Text(error,
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.red
+              ),
+            ),
+            TextButton(onPressed: () {
+              Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const BottomNavBar()), (route) => false
+              );
+            }, child: const Text("다음 단계로")),
+          ],
         ),
       ),
     );
