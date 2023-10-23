21d9ba9830858b9503965c9988f86a02bf948437
melonpanna <prairial6@gmail.com>
2023-10-23 16:13:58 +0900

    :zap: feat: serviceImpl 추가

--- backend/src/main/java/com/dollyanddot/kingmaker/domain/notification/service/NotificationServiceImpl.java
+++ backend/src/main/java/com/dollyanddot/kingmaker/domain/notification/service/NotificationServiceImpl.java
@@ -1,8 +1,9 @@
 package com.dollyanddot.kingmaker.domain.notification.service;
 
-import com.dollyanddot.kingmaker.domain.notification.repository.NotificationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-public interface NotificationService {
+@Service
+@RequiredArgsConstructor
+public class NotificationServiceImpl {
 }
