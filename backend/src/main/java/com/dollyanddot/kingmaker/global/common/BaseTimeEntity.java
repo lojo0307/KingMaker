370e41500a0a1e0cba10fdd3bdd79df533b32bb3
허성백 <heosb123@hanmail.net>
2023-10-23 14:21:01 +0900

    :pencil2: fix: BaseTimeEntity edited

--- backend/src/main/java/com/dollyanddot/kingmaker/global/common/BaseTimeEntity.java
+++ backend/src/main/java/com/dollyanddot/kingmaker/global/common/BaseTimeEntity.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
@@ -13,7 +14,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @SuperBuilder
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseTimeEntity {
