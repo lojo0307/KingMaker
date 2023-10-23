955c0894908414b574889a3be48b782120b69ef2
허성백 <heosb123@hanmail.net>
2023-10-23 10:50:30 +0900

    :tada: init: common resources update

---
+++ backend/src/main/java/com/dollyanddot/kingmaker/global/common/EnvelopeResponse.java
@@ -0,0 +1,21 @@
+package com.dollyanddot.kingmaker.global.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class EnvelopeResponse<T> {
+
+  @Builder.Default
+  private String code = "200";
+
+  @Builder.Default
+  private String message = "success";
+
+  private T data;
+}
