aa74e7977464ca6bbaaa9351bc42b051022fdcde
melonpanna <prairial6@gmail.com>
2023-10-23 17:41:13 +0900

    :zap: feat: todo 삭제 api

--- backend/src/main/java/com/dollyanddot/kingmaker/domain/todo/service/TodoService.java
+++ backend/src/main/java/com/dollyanddot/kingmaker/domain/todo/service/TodoService.java
@@ -1,4 +1,5 @@
 package com.dollyanddot.kingmaker.domain.todo.service;
 
 public interface TodoService {
+    void deleteTodoByTodoId(Long todoId);
 }
--- backend/src/main/java/com/dollyanddot/kingmaker/domain/todo/service/TodoServiceImpl.java
+++ backend/src/main/java/com/dollyanddot/kingmaker/domain/todo/service/TodoServiceImpl.java
@@ -1,9 +1,21 @@
 package com.dollyanddot.kingmaker.domain.todo.service;
 
+import com.dollyanddot.kingmaker.domain.todo.domain.Todo;
+import com.dollyanddot.kingmaker.domain.todo.exception.NonExistTodoIdException;
+import com.dollyanddot.kingmaker.domain.todo.repository.TodoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
-public class TodoServiceImpl {
+public class TodoServiceImpl implements TodoService{
+    private final TodoRepository todoRepository;
+    @Override
+    public void deleteTodoByTodoId(Long todoId) throws NonExistTodoIdException {
+        Optional<Todo> temp=todoRepository.findById(todoId);
+        if(temp.isEmpty())throw new NonExistTodoIdException();
+        todoRepository.deleteTodoByTodoId(todoId);
+    }
 }
