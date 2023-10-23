48e57018057b6d29f6d8f4191d509baaf337c714
lojo0307 <lojo0307@naver.com>
2023-10-23 15:23:46 +0900

    :zap: feat: auth 관련 엔티티 정의

---
+++ backend/src/main/java/com/dollyanddot/kingmaker/domain/auth/domain/Gender.java
@@ -0,0 +1,5 @@
+package com.dollyanddot.kingmaker.domain.auth.domain;
+
+public enum Gender {
+    MAN, WOMAN
+}
---
+++ backend/src/main/java/com/dollyanddot/kingmaker/domain/auth/domain/Member.java
@@ -0,0 +1,40 @@
+package com.dollyanddot.kingmaker.domain.auth.domain;
+
+import com.dollyanddot.kingmaker.domain.kingdom.Kingdom;
+import com.dollyanddot.kingmaker.domain.auth.domain.Credential;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long memberId;
+
+    @OneToOne
+    @JoinColumn(name = "credential_id", nullable = false)
+    private Credential credential;
+
+    @OneToOne
+    @JoinColumn(name = "kingdom_id", nullable = false)
+    private Kingdom kingdom;
+
+    @Column(nullable = false, length = 10)
+    private String nickname;
+
+    @Column(nullable = false)
+    private Gender gender;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+}
