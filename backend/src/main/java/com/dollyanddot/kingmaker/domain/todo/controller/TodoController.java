22ae14019030412ce75b590f872bf9743bfb2c41
melonpanna <prairial6@gmail.com>
2023-10-23 17:17:16 +0900

    :zap: feat: todo 도메인 controller, repository, service 파일 생성

---
+++ backend/src/main/java/com/dollyanddot/kingmaker/domain/todo/controller/TodoController.java
@@ -0,0 +1,11 @@
+package com.dollyanddot.kingmaker.domain.todo.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/todo")
+public class TodoController {
+}
