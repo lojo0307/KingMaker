be2737d39bfa72f4d423ae3077e49b8d51d43c13
KimByeongChan <qudcks8749@naver.com>
2023-10-25 10:16:19 +0900

    :pencil2: fix: homepage hotfix

--- frontend/kingmaker/lib/page/home_page.dart
+++ frontend/kingmaker/lib/page/home_page.dart
@@ -11,16 +11,11 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-<<<<<<< 8d4b2c4c3006cab2ee31141ee60e002cdd11d41a
     return Align(
       alignment: Alignment.center,
       child: GameWidget.controlled(
         gameFactory:()=> MyGame(context),
       ),
-=======
-    return Container(
-      color: Colors.green,
->>>>>>> 40867789c7f58a27f67b6a90410d0926b4957083
     );
   }
 }
