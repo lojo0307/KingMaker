e2d357b22279547d9d9ea973a36e3dec485b20b2
melonpanna <prairial6@gmail.com>
2023-10-25 10:47:39 +0900

    :sparkles: feat: 할 일 상세조회 api 완료

--- backend/src/main/java/com/dollyanddot/kingmaker/domain/todo/repository/TodoRepository.java
+++ backend/src/main/java/com/dollyanddot/kingmaker/domain/todo/repository/TodoRepository.java
@@ -1,6 +1,7 @@
 package com.dollyanddot.kingmaker.domain.todo.repository;
 
 import com.dollyanddot.kingmaker.domain.todo.domain.Todo;
+import com.dollyanddot.kingmaker.domain.todo.dto.response.TodoDetailResDto;
 import com.dollyanddot.kingmaker.domain.todo.dto.response.TodoListResDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -15,4 +16,6 @@ public interface TodoRepository extends JpaRepository<Todo,Long> {
 
     @Query(name = "getTodoList", nativeQuery = true)
     List<TodoListResDto> getTodoList(Long memberId,LocalDate targetDate);
+
+    Optional<Todo> getTodoByTodoId(Long todoId);
 }
