eca8133200af02abb4cc5f78143bbf0a09e96e32
허성백 <heosb123@hanmail.net>
2023-10-30 15:09:07 +0900

    :pencil2: fix: BaseTimeEntity 자동으로 시간 저장할 수 있도록 @EnableJpaAuditing 어노테이션 추가

--- backend/src/main/java/com/dollyanddot/kingmaker/KingmakerApplication.java
+++ backend/src/main/java/com/dollyanddot/kingmaker/KingmakerApplication.java
@@ -2,8 +2,10 @@ package com.dollyanddot.kingmaker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class KingmakerApplication {
 
   public static void main(String[] args) {
