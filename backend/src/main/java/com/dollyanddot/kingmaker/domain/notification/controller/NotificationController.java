e374b49d0319d13e65acbd2b40a46487b85f6aab
melonpanna <prairial6@gmail.com>
2023-10-30 14:18:37 +0900

    :zap: feat: 알림 탭 조회 기능 추가

--- backend/src/main/java/com/dollyanddot/kingmaker/domain/notification/controller/NotificationController.java
+++ backend/src/main/java/com/dollyanddot/kingmaker/domain/notification/controller/NotificationController.java
@@ -1,11 +1,40 @@
 package com.dollyanddot.kingmaker.domain.notification.controller;
 
+import com.dollyanddot.kingmaker.domain.calendar.dto.response.CalendarAchieveAndSumResDto;
+import com.dollyanddot.kingmaker.domain.notification.domain.Notification;
+import com.dollyanddot.kingmaker.domain.notification.dto.response.NotificationResDto;
+import com.dollyanddot.kingmaker.domain.notification.service.NotificationService;
+import com.dollyanddot.kingmaker.global.common.EnvelopeResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/notification")
 public class NotificationController {
+    private final NotificationService notificationService;
+
+    //1분마다 일정 알림 발송하기 위한 함수 테스트용
+    @GetMapping("test")
+    void sendTest(){
+        notificationService.sendNotification();
+        return;
+    }
+
+    @GetMapping("{memberId}")
+    public EnvelopeResponse<List<NotificationResDto>> getNotification(@PathVariable Long memberId){
+        List<NotificationResDto> list=notificationService.getNotification(memberId)
+                .stream()
+                .map(m->new NotificationResDto(m.getMessage(),m.getNotificationType().getNotificationTypeId(),m.getNotificationId()))
+                .collect(Collectors.toList());
+        return EnvelopeResponse.<List<NotificationResDto>>builder()
+                .data(list)
+                .build();
+    }
 }
--- backend/src/main/java/com/dollyanddot/kingmaker/domain/notification/service/NotificationService.java
+++ backend/src/main/java/com/dollyanddot/kingmaker/domain/notification/service/NotificationService.java
@@ -1,5 +1,12 @@
 package com.dollyanddot.kingmaker.domain.notification.service;
 
+import com.dollyanddot.kingmaker.domain.notification.domain.Notification;
+import com.dollyanddot.kingmaker.domain.notification.dto.response.NotificationResDto;
+
+import java.util.List;
+
 public interface NotificationService {
     void sendNotification();
+
+    List<Notification> getNotification(Long memberId);
 }
