97d394ee906e31ccd885b7d6ec45e6219bcd8952
melonpanna <prairial6@gmail.com>
2023-10-30 17:20:34 +0900

    :sparkles: feat: 아침/저녁 알림 생성 api-fcm 발송 제외 부분 완료

---
+++ backend/src/main/java/com/dollyanddot/kingmaker/domain/calendar/dto/CountPlanDto.java
@@ -0,0 +1,13 @@
+package com.dollyanddot.kingmaker.domain.calendar.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CountPlanDto {
+    Long memberId;
+    Long cnt;
+}
--- backend/src/main/java/com/dollyanddot/kingmaker/domain/notification/controller/NotificationController.java
+++ backend/src/main/java/com/dollyanddot/kingmaker/domain/notification/controller/NotificationController.java
@@ -6,10 +6,7 @@ import com.dollyanddot.kingmaker.domain.notification.dto.response.NotificationRe
 import com.dollyanddot.kingmaker.domain.notification.service.NotificationService;
 import com.dollyanddot.kingmaker.global.common.EnvelopeResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,7 +20,9 @@ public class NotificationController {
     //1분마다 일정 알림 발송하기 위한 함수 테스트용
     @GetMapping("test")
     void sendTest(){
-        notificationService.sendNotification();
+//        notificationService.sendNotification();
+//        notificationService.sendMorningNotification();
+        notificationService.sendEveningNotification();
         return;
     }
 
@@ -36,5 +35,11 @@ public class NotificationController {
         return EnvelopeResponse.<List<NotificationResDto>>builder()
                 .data(list)
                 .build();
+    }
+
+    @DeleteMapping("{notificationId}")
+    public EnvelopeResponse<Void> deleteNotification(@PathVariable Long notificationId){
+        notificationService.deleteNotification(notificationId);
+        return EnvelopeResponse.<Void>builder().build();
     }
 }
