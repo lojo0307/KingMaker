3af537eac2ea0c75d577494b2e6255bd58ae9436
Seulgi <urina842@gmail.com>
2023-10-31 10:50:44 +0900

    :zap: feat: @Transactional 추가

--- backend/src/main/java/com/dollyanddot/kingmaker/domain/member/service/MemberService.java
+++ backend/src/main/java/com/dollyanddot/kingmaker/domain/member/service/MemberService.java
@@ -6,6 +6,7 @@ import com.dollyanddot.kingmaker.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -14,12 +15,12 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Transactional
     public void updateNickname(NicknameDto nicknameDto) {
         Member member
             = memberRepository.findById(nicknameDto.getMemberId()).orElseThrow();
 
         member.setNickname(nicknameDto.getNickname());
-        memberRepository.save(member);
     }
 
 }
