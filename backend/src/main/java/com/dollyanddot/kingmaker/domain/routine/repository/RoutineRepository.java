cb71ceebccbfe30b09e5d4d351d2783ffec2cdce
허성백 <heosb123@hanmail.net>
2023-11-01 13:38:43 +0900

    Merge: 오늘의 루틴 자동 등록하는 기능 스케줄러 추가

--- backend/src/main/java/com/dollyanddot/kingmaker/domain/routine/repository/RoutineRepository.java
+++ backend/src/main/java/com/dollyanddot/kingmaker/domain/routine/repository/RoutineRepository.java
@@ -1,8 +1,16 @@
 package com.dollyanddot.kingmaker.domain.routine.repository;
 
 import com.dollyanddot.kingmaker.domain.routine.domain.Routine;
+import java.time.LocalDateTime;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface RoutineRepository extends JpaRepository<Routine, Long> {
 
+  @Query(value = "SELECT r "
+      + "FROM routine r "
+      + "WHERE date_format(r.registerAt, '%Y-%M-%d') = date_format(:today, '%Y-%M-%d') "
+      + "OR r.registerDay = :day")
+  List<Routine> findAllByRegisterDateOrDay(LocalDateTime today, int day);
 }
