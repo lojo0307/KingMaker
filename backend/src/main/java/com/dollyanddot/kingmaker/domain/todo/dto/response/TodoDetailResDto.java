d1dcd324f627be2253d21fa46acbc28d281ddde6
melonpanna <prairial6@gmail.com>
2023-11-01 17:32:28 +0900

    :art: refactor: 코드 setter에서 builder 사용 수정

--- backend/src/main/java/com/dollyanddot/kingmaker/domain/todo/dto/response/TodoDetailResDto.java
+++ backend/src/main/java/com/dollyanddot/kingmaker/domain/todo/dto/response/TodoDetailResDto.java
@@ -1,6 +1,7 @@
 package com.dollyanddot.kingmaker.domain.todo.dto.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class TodoDetailResDto {
     String todoNm;
     String todoPlace;
