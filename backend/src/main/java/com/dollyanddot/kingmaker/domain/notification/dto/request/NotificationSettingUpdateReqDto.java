321efa78346b95735e1969dbda7fbe151f7d3f11
melonpanna <prairial6@gmail.com>
2023-11-01 17:23:58 +0900

    :zap: feat: 회원가입 시 알림 초기 설정 api

--- backend/src/main/java/com/dollyanddot/kingmaker/domain/notification/dto/request/NotificationSettingUpdateReqDto.java
+++ backend/src/main/java/com/dollyanddot/kingmaker/domain/notification/dto/request/NotificationSettingUpdateReqDto.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class NotificationSettingReqDto {
+public class NotificationSettingUpdateReqDto {
+    Long memberId;
     int notificationTypeId;
-    boolean activatedYn;
 }
