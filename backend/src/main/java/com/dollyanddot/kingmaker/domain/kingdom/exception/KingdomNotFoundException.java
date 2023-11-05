52e3127f6f9dddd0dcaf201cc7fb4cc1cde6514d
seul <l.23.53.l2l9@gmail.com>
2023-11-05 20:14:40 +0900

    :pencil2: fix: 에러 코드 수정

--- backend/src/main/java/com/dollyanddot/kingmaker/domain/kingdom/exception/KingdomNotFoundException.java
+++ backend/src/main/java/com/dollyanddot/kingmaker/domain/kingdom/exception/KingdomNotFoundException.java
@@ -4,9 +4,8 @@ import com.dollyanddot.kingmaker.global.exception.domain.NotFoundException;
 
 public class KingdomNotFoundException extends NotFoundException {
 
-    //TODO: code 및 msg 정하기
     public KingdomNotFoundException() {
-        super("1005", "왕국을 찾을 수 없습니다.");
+        super("604", "왕국을 찾을 수 없습니다.");
     }
 
 }
--- backend/src/main/java/com/dollyanddot/kingmaker/domain/member/exception/MemberNotFoundException.java
+++ backend/src/main/java/com/dollyanddot/kingmaker/domain/member/exception/MemberNotFoundException.java
@@ -4,9 +4,8 @@ import com.dollyanddot.kingmaker.global.exception.domain.NotFoundException;
 
 public class MemberNotFoundException extends NotFoundException {
 
-    //TODO: code 및 msg 정하기
     public MemberNotFoundException() {
-        super("1004", "사용자를 찾을 수 없습니다.");
+        super("704", "사용자를 찾을 수 없습니다.");
     }
 
 }
