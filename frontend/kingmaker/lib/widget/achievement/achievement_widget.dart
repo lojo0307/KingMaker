056117597c7c8688fd327950b4986f97ffca3349
Byeonggu <qudcks8749@naver.com>
2023-11-06 23:55:44 +0900

    :zap: feat: 업적페이지 정렬 및 달성 업적/미달성 업적 구분

--- frontend/kingmaker/lib/widget/achievement/achievement_widget.dart
+++ frontend/kingmaker/lib/widget/achievement/achievement_widget.dart
@@ -11,12 +11,17 @@ class AchievementWidget extends StatefulWidget {
 class _AchievementWidgetState extends State<AchievementWidget> {
   @override
   Widget build(BuildContext context) {
-    print('${widget.data['reward_nm']}');
+    // 위젯의 'achieved_yn' 값에 따라 배경색과 텍스트 내용을 결정합니다.
+    final bool isAchieved = widget.data['achieved_yn'] == '1';
+    final Color bgColor = isAchieved ? Colors.white : Colors.grey;
+    final String? rewardNmtText = isAchieved ? widget.data['reward_nm'] : "미달성 업적";
+    final String? rewardContText = isAchieved ? widget.data['reward_msg'] : widget.data['reward_cont'];
+
     return Container(
       height: 80,
       margin: EdgeInsets.only(bottom: 6),
       decoration: BoxDecoration(
-        // color: Colors.red,
+        color: bgColor, // 조건에 따른 배경색 설정
         border: Border.all(width: 1),
         borderRadius: BorderRadius.all(Radius.circular(10)),
       ),
@@ -35,8 +40,8 @@ class _AchievementWidgetState extends State<AchievementWidget> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text('${widget.data['reward_nm']}', style: TextStyle(fontSize: 20)),
-                    Text('${widget.data['reward_cont']}')
+                    Text('${rewardNmtText}', style: TextStyle(fontSize: 20)),
+                    Text('${rewardContText}') // 조건에 따른 텍스트 내용 설정
                   ],
                 ),
               )
