e6ab60f7cfe137ef515f00a087eaee320198909c
Seulgi <urina842@gmail.com>
2023-11-06 12:14:56 +0900

    :zap: feat: RewardInfoDto 추가

---
+++ backend/src/main/java/com/dollyanddot/kingmaker/domain/reward/dto/RewardInfoDto.java
@@ -0,0 +1,19 @@
+package com.dollyanddot.kingmaker.domain.reward.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class RewardInfoDto {
+
+    private int rewardId;
+    private String rewardNm;
+    private String rewardCond;
+    private String rewardMsg;
+
+    public static RewardInfoDto from(int rewardId, String rewardNm, String rewardCont, String rewardMsg) {
+        return new RewardInfoDto(rewardId, rewardNm, rewardCont, rewardMsg);
+    }
+
+}
