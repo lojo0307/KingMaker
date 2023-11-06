b7959b9265eab2d2ded5a870c3d369644592a67b
lojo0307 <lojo0307@naver.com>
2023-11-06 15:38:15 +0900

    Feat: 회원탈퇴 api 추가

--- backend/src/main/java/com/dollyanddot/kingmaker/domain/member/controller/MemberController.java
+++ backend/src/main/java/com/dollyanddot/kingmaker/domain/member/controller/MemberController.java
@@ -9,10 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -30,6 +27,12 @@ public class MemberController {
         LoginResDto loginResDto = memberService.signUp(credentialId, signUpReqDto);
 
         return ResponseEntity.ok().body(loginResDto);
+    }
+
+    @DeleteMapping("/leave/{memberId}")
+    public ResponseEntity<Void> withdraw(@PathVariable long memberId) {
+        memberService.withDraw(memberId);
+        return ResponseEntity.ok().build();
     }
 
     @PostMapping("notification/setting")
