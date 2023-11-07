8bb304e7a7bca1770b2411062dcbc8ed185c77cc
KimByeongChan <qudcks8749@naver.com>
2023-11-07 10:56:36 +0900

    :zap: feat: 업적페이지 상세내용 넣기위한 위젯 뒷면 구현

--- frontend/kingmaker/lib/widget/achievement/achievement_widget.dart
+++ frontend/kingmaker/lib/widget/achievement/achievement_widget.dart
@@ -21,31 +21,30 @@ class _AchievementWidgetState extends State<AchievementWidget> {
     final String? rewardContText = isAchieved ? widget.data['reward_msg'] : widget.data['reward_cont'];
     final String rewardDateText = isAchieved ? '업적 달성일 : ${widget.data['modified_at']}' : " ";
     final con = FlipCardController();
-    return Container(
-      height: 80,
-      margin: EdgeInsets.only(bottom: 6),
-      decoration: BoxDecoration(
-        color: bgColor, // 조건에 따른 배경색 설정
-        border: Border.all(width: 1),
-        borderRadius: BorderRadius.all(Radius.circular(10)),
-      ),
-      child: Row(
-        children: [
-          const Flexible(
-              child: Image(
-                image: AssetImage('assets/achievement/sample.png'),
-                height: 100,
-                width: 100,
-                // color: Colors.red,
-              )
+    return FlipCard(
+        controller: con,
+        rotateSide: RotateSide.right,
+        onTapFlipping: true,
+        axis: FlipAxis.horizontal,
+        frontWidget: Container(
+          height: 80,
+          margin: EdgeInsets.only(bottom: 6),
+          decoration: BoxDecoration(
+            color: bgColor, // 조건에 따른 배경색 설정
+            border: Border.all(width: 1),
+            borderRadius: BorderRadius.all(Radius.circular(10)),
           ),
-          Flexible(
-              child:FlipCard(
-                rotateSide: RotateSide.right,
-                onTapFlipping: true,
-                axis: FlipAxis.horizontal,
-                controller: con,
-                frontWidget: Center(
+          child: Row(
+            children: [
+              const Flexible(
+                  child: Image(
+                    image: AssetImage('assets/achievement/sample.png'),
+                    height: 100,
+                    width: 100,
+                    // color: Colors.red,
+                  )
+              ),
+              Flexible(
                   child: Container(
                     // decoration: BoxDecoration(color: Colors.blue),
                     padding: EdgeInsets.only(top: 10),
@@ -58,26 +57,44 @@ class _AchievementWidgetState extends State<AchievementWidget> {
                       ],
                     ),
                   )
-                ),
-                backWidget: Center(
+              ),
+            ],
+          ),
+        ),
+        backWidget: Container(
+          height: 80,
+          margin: EdgeInsets.only(bottom: 6),
+          decoration: BoxDecoration(
+            color: bgColor, // 조건에 따른 배경색 설정
+            border: Border.all(width: 1),
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+          ),
+          child: Row(
+            children: [
+              Flexible(
                   child: Container(
                     // decoration: BoxDecoration(color: Colors.blue),
                     padding: EdgeInsets.only(top: 10),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Text('${rewardNmtText}', style: TextStyle(fontSize: 20)),//업적이름/미달성 업적
-                        Text('뒷면 입니당', style: TextStyle(fontSize: 20)), // 조건에 따른 텍스트 내용 설정
-                        // Text('업적 달성일 : ${rewardDateText}',  style: TextStyle(fontSize: 10)),
-
+                        // Text('${rewardNmtText}', style: TextStyle(fontSize: 20)),//업적이름/미달성 업적
+                        Center(
+                          child: Text('${rewardContText}',
+                                style: TextStyle(fontSize: 30,
+                                      color: Colors.black)
+                                  ),
+                        )
+                        // Text('${rewardDateText}',  style: TextStyle(fontSize: 10)),
                       ],
                     ),
-                  ),
-                )
+                  )
               ),
-          )
-        ],
-      ),
+            ],
+          ),
+        ),
+
     );
   }
 }
