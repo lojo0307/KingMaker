8fcee5cb38acefbedf1eaa4a5714b583af25d7d7
Seulgi <urina842@gmail.com>
2023-11-08 21:50:28 +0900

    :zap: feat: 가장 많은/적은 루틴 카테고리 메서드

--- backend/src/main/java/com/dollyanddot/kingmaker/domain/routine/repository/RoutineRepositoryCustom.java
+++ backend/src/main/java/com/dollyanddot/kingmaker/domain/routine/repository/RoutineRepositoryCustom.java
@@ -1,7 +1,12 @@
 package com.dollyanddot.kingmaker.domain.routine.repository;
 
+import com.dollyanddot.kingmaker.domain.category.dto.response.CategoryCntResDto;
 import java.time.LocalDateTime;
 
 public interface RoutineRepositoryCustom {
     LocalDateTime findMostRecentAchieved();
+
+    CategoryCntResDto getMinRoutineCategory(Long memberId);
+
+    CategoryCntResDto getMaxRoutineCategory(Long memberId);
 }
