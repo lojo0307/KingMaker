d7cb642552996bd85f1a75e7d1989532633da49b
jht043 <jht043@naver.com>
2023-11-08 17:42:34 +0900

    :zap: feat: 카카오로그인

--- frontend/kingmaker/lib/api/social_api.dart
+++ frontend/kingmaker/lib/api/social_api.dart
@@ -1,13 +1,28 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:dio/dio.dart';
-import 'dart:math';
-
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 class SocialApi{
-  Future<String> kakaologin() async {
-    String token = "";
-    // 인가코드
-
-    return token;
+  Future<String?> kakaologin() async {
+    try {
+      bool isInstalled = await isKakaoTalkInstalled();
+      if (isInstalled) { // 카톡이 있는 경우
+        try {
+          OAuthToken token = await UserApi.instance.loginWithKakaoTalk();
+          print(token.accessToken);
+          return token.accessToken;
+        } catch (e) {
+          return null;
+        }
+      } else { // 카톡이 없는 경우
+        try {
+          OAuthToken token = await UserApi.instance.loginWithKakaoAccount();
+          print(token.accessToken);
+          return token.accessToken;
+        } catch (e) {
+          return null;
+        }
+      }
+    } catch (e) {
+      return null;
+    }
   }
   Future<String> googlelogin() async {
     String token = "";
