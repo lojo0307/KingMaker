b08d956e7973355599b899b2187f829d68fbffc2
Seulgi <urina842@gmail.com>
2023-11-08 16:08:20 +0900

    :zap: feat: 오늘의 처리해야 할 몬스터 개수 api 추가

--- backend/src/main/java/com/dollyanddot/kingmaker/domain/member/controller/MypageController.java
+++ backend/src/main/java/com/dollyanddot/kingmaker/domain/member/controller/MypageController.java
@@ -1,12 +1,12 @@
 package com.dollyanddot.kingmaker.domain.member.controller;
 
 import com.dollyanddot.kingmaker.domain.calendar.dto.response.CalendarStreakResDto;
+import com.dollyanddot.kingmaker.domain.calendar.service.CalendarService;
 import com.dollyanddot.kingmaker.domain.member.dto.response.RewardListResDto;
 import com.dollyanddot.kingmaker.domain.reward.service.RewardService;
 import com.dollyanddot.kingmaker.domain.todo.service.TodoService;
 import com.dollyanddot.kingmaker.global.common.EnvelopeResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,6 +17,7 @@ import java.util.List;
 public class MypageController {
     private final TodoService todoService;
     private final RewardService rewardService;
+    private final CalendarService calendarService;
 
     @GetMapping("calendar/{memberId}")
     public EnvelopeResponse<List<CalendarStreakResDto>> getTodoCalendar(@PathVariable Long memberId, @RequestParam int year, @RequestParam int month){
@@ -29,5 +30,10 @@ public class MypageController {
     @GetMapping("/reward/{memberId}")
     public EnvelopeResponse<List<RewardListResDto>> getRewardList(@PathVariable long memberId) {
         return EnvelopeResponse.<List<RewardListResDto>>builder().data(rewardService.getRewardList(memberId)).build();
+    }
+
+    @GetMapping("/plan/{memberId}")
+    public EnvelopeResponse<Long> getDailyMonsterCnt(@PathVariable Long memberId) {
+        return EnvelopeResponse.<Long>builder().data(calendarService.getDailyMonsterCnt(memberId)).build();
     }
 }
