9f606b6158f4fc22b6b67d92a7a4518eff44b477
전현태 <jht043@naver.com>
2023-11-10 12:26:25 +0900

    Merge: 로그인시 오류 수정

--- frontend/kingmaker/lib/api/social_api.dart
+++ frontend/kingmaker/lib/api/social_api.dart
@@ -7,10 +7,10 @@ class SocialApi{
   Future<String?> kakaologin() async {
     try {
       bool isInstalled = await isKakaoTalkInstalled();
+      print('API');
       if (isInstalled) { // 카톡이 있는 경우
         try {
           OAuthToken token = await UserApi.instance.loginWithKakaoTalk();
-          print(token.accessToken);
           return token.accessToken;
         } catch (e) {
           return null;
@@ -18,7 +18,6 @@ class SocialApi{
       } else { // 카톡이 없는 경우
         try {
           OAuthToken token = await UserApi.instance.loginWithKakaoAccount();
-          print(token.accessToken);
           return token.accessToken;
         } catch (e) {
           return null;
@@ -38,30 +37,26 @@ class SocialApi{
     try {
       final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
       final GoogleSignInAuthentication googleSignInAuthentication = await googleUser!.authentication;
-      print('accessToken : ${googleSignInAuthentication.accessToken}');
       // 인증 코드가 있는지 확인하고, 없다면 사용자에게 필요한 권한을 요청함
       if (googleUser == null) {
         // 사용자가 로그인을 취소한 경우
-        print('로그인 취소');
-        return "ddd";
+        return null;
       } else {
         // serverAuthCode가 null인 경우 사용자에게 다시 권한을 요청해야 함
         if (googleSignInAuthentication == null) {
           // 사용자가 필요한 권한을 부여하지 않았을 때의 처리
           await googleSignIn.signOut();
           print('Required consent not granted by user');
-          return "ddd";
+          return null;
         } else {
           //accessToken을 저장
-          print(googleSignInAuthentication.accessToken);
-
           return googleSignInAuthentication.accessToken;
         }
       }
     } catch (error) {
       // 오류 처리
       print('Error while signing in with Google: $error');
-      return 'Failed to sign in with Google';
+      return null;
     }
   }
 }
