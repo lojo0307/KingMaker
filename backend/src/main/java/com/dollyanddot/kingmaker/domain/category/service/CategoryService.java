253ccb99c0e908a3b9e42c881a0fb3b19eab7f74
Seulgi <urina842@gmail.com>
2023-11-10 09:01:59 +0900

    :zap: feat: 가장 많이/적게 달성한 카테고리 메서드

--- backend/src/main/java/com/dollyanddot/kingmaker/domain/category/service/CategoryService.java
+++ backend/src/main/java/com/dollyanddot/kingmaker/domain/category/service/CategoryService.java
@@ -1,7 +1,9 @@
 package com.dollyanddot.kingmaker.domain.category.service;
 
+import com.dollyanddot.kingmaker.domain.category.domain.Category;
 import com.dollyanddot.kingmaker.domain.category.dto.CategoryDto;
 import com.dollyanddot.kingmaker.domain.category.dto.response.CategoryCntResDto;
+import com.dollyanddot.kingmaker.domain.category.exception.CategoryNotFoundException;
 import com.dollyanddot.kingmaker.domain.category.repository.CategoryRepository;
 import com.dollyanddot.kingmaker.domain.routine.repository.RoutineRepository;
 import com.dollyanddot.kingmaker.domain.todo.repository.TodoRepository;
@@ -20,50 +22,76 @@ public class CategoryService {
 
     public CategoryDto getMinCategory(Long memberId) {
         CategoryCntResDto todo = todoRepository.getMinTodoCategory(memberId);
-        if(todo!=null) {
-            log.info("번호: {}", todo.getCategoryId());
-            log.info("개수: {}", todo.getCnt());
-            log.info("수정일: {}", todo.getModifiedAt());
-        }
+        log.info("todo 번호: {}", todo.getCategoryId());
+        log.info("todo 개수: {}", todo.getCnt());
+        log.info("todo 수정일: {}", todo.getModifiedAt());
 
         CategoryCntResDto routine = routineRepository.getMinRoutineCategory(memberId);
-        if(routine!=null) {
-            log.info("번호: {}", routine.getCategoryId());
-            log.info("개수: {}", routine.getCnt());
-            log.info("수정일: {}", routine.getModifiedAt());
+        log.info("routine 번호: {}", routine.getCategoryId());
+        log.info("routine 개수: {}", routine.getCnt());
+        log.info("routine 수정일: {}", routine.getModifiedAt());
+
+        //둘 다 달성한 개수가 없는 경우
+        if(todo.getCnt()==-1 && routine.getCnt()==-1) {
+            return CategoryDto.builder().categoryId(-1L).categoryNm(
+                "달성한 일정이 없음").build();
         }
 
-        //카테고리 예외 처리?
-        if(todo==null && routine!=null) {
-            return CategoryDto.from(categoryRepository.findById(routine.getCategoryId()).get());
-        } else if(todo!=null && routine==null) {
-            return CategoryDto.from(categoryRepository.findById(todo.getCategoryId()).get());
-        } else if(todo!=null && routine!=null) {
-            int comparison = (todo.getCnt() == routine.getCnt())
-                ? todo.getModifiedAt().compareTo(routine.getModifiedAt())
-                : Long.compare(todo.getCnt(), routine.getCnt());
-        } else if(todo==null && routine==null) {
+        Category category;
+        //둘 중에 하나라도 달성한 게 있으면 개수 비교
+        if(todo.getCnt()==routine.getCnt()) {
+            //최신순
+            if(todo.getModifiedAt().isAfter(routine.getModifiedAt())) {
+                category = categoryRepository.findById(
+                    todo.getCategoryId()).orElseThrow(CategoryNotFoundException::new);
+            } else {
+                category = categoryRepository.findById(
+                    routine.getCategoryId()).orElseThrow(CategoryNotFoundException::new);
+            }
+        } else {
+            Long categoryId = todo.getCnt()<routine.getCnt()? todo.getCategoryId() : routine.getCategoryId();
+            category = categoryRepository.findById(categoryId).orElseThrow(CategoryNotFoundException::new);
         }
 
-        return null;
+        return CategoryDto.builder().categoryId(category.getId()).categoryNm(
+            category.getName()).build();
     }
 
     public CategoryDto getMaxCategory(Long memberId) {
-        CategoryCntResDto categoryTodoCntResDto = todoRepository.getMaxTodoCategory(memberId);
-        if(categoryTodoCntResDto!=null) {
-            log.info("번호: {}", categoryTodoCntResDto.getCategoryId());
-            log.info("개수: {}", categoryTodoCntResDto.getCnt());
-            log.info("수정일: {}", categoryTodoCntResDto.getModifiedAt());
+        CategoryCntResDto todo = todoRepository.getMaxTodoCategory(memberId);
+        log.info("todo 번호: {}", todo.getCategoryId());
+        log.info("todo 개수: {}", todo.getCnt());
+        log.info("todo 수정일: {}", todo.getModifiedAt());
+
+        CategoryCntResDto routine = routineRepository.getMaxRoutineCategory(memberId);
+        log.info("routine 번호: {}", routine.getCategoryId());
+        log.info("routine 개수: {}", routine.getCnt());
+        log.info("routine 수정일: {}", routine.getModifiedAt());
+
+        //둘 다 달성한 개수가 없는 경우
+        if(todo.getCnt()==-1 && routine.getCnt()==-1) {
+            return CategoryDto.builder().categoryId(-1L).categoryNm(
+                "달성한 일정이 없음").build();
         }
 
-        CategoryCntResDto categoryRoutineCntResDto = routineRepository.getMaxRoutineCategory(memberId);
-        if(categoryRoutineCntResDto!=null) {
-            log.info("번호: {}", categoryRoutineCntResDto.getCategoryId());
-            log.info("개수: {}", categoryRoutineCntResDto.getCnt());
-            log.info("수정일: {}", categoryRoutineCntResDto.getModifiedAt());
+        Category category;
+        //둘 중에 하나라도 달성한 게 있으면 개수 비교
+        if(todo.getCnt()==routine.getCnt()) {
+            //최신순
+            if(todo.getModifiedAt().isAfter(routine.getModifiedAt())) {
+                category = categoryRepository.findById(
+                    todo.getCategoryId()).orElseThrow(CategoryNotFoundException::new);
+            } else {
+                category = categoryRepository.findById(
+                    routine.getCategoryId()).orElseThrow(CategoryNotFoundException::new);
+            }
+        } else {
+            Long categoryId = todo.getCnt()>routine.getCnt()? todo.getCategoryId() : routine.getCategoryId();
+            category = categoryRepository.findById(categoryId).orElseThrow(CategoryNotFoundException::new);
         }
 
-        return null;
+        return CategoryDto.builder().categoryId(category.getId()).categoryNm(
+            category.getName()).build();
     }
 
 }
