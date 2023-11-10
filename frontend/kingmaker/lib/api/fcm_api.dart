b5339a298229f0481492d467bc15bd275205791a
melonpanna <prairial6@naver.com>
2023-11-10 16:33:56 +0900

    :zap: feat: 토큰 삭제 함수

--- frontend/kingmaker/lib/api/fcm_api.dart
+++ frontend/kingmaker/lib/api/fcm_api.dart
@@ -3,14 +3,35 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:dio/dio.dart';
 
 class FcmApi{
-  Future<String?> generateFcmToken(){
+  Future<String?> generateFcmToken() async{
     //알림 허용
+    FirebaseMessaging messaging = FirebaseMessaging.instance;
 
-    return getFcmToken();
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      badge: true,
+      sound: true,
+      provisional: false,
+    );
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      return getFcmToken();
+    }
+    else{
+      print("권한 요청이 거절됨");
+      return null;
+    }
   }
 
   Future<String?> getFcmToken() async {
     String? _fcmToken = await FirebaseMessaging.instance.getToken();
     return _fcmToken;
   }
+
+  void deleteFcmToken(){
+    FirebaseMessaging.instance.deleteToken();
+    return;
+  }
+  
+  //TODO: 메시지 수신 시 처리하는 함수
 }
