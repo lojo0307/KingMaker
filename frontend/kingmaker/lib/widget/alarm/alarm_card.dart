a6bd2132915950dfee38034fc777b4801418300b
jht043 <jht043@naver.com>
2023-11-13 14:52:03 +0900

    :zap: feat: 알림페이지 API연동

--- frontend/kingmaker/lib/widget/alarm/alarm_card.dart
+++ frontend/kingmaker/lib/widget/alarm/alarm_card.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:kingmaker/consts/colors.dart';
+import 'package:kingmaker/dto/alarm_dto.dart';
+import 'package:kingmaker/provider/alarm_provider.dart';
+import 'package:kingmaker/provider/member_provider.dart';
+import 'package:provider/provider.dart';
 class AlarmCard extends StatelessWidget {
   const AlarmCard({super.key, required this.data, required this.delFlag});
-  final Map<String, String> data;
+  final AlarmDto data;
   final delFlag;
+  static const type = ['전체', '아침 알림' ,'할일 알림', '루틴 알림', '저녁 알림'];
   @override
   Widget build(BuildContext context) {
     return
@@ -34,19 +39,23 @@ class AlarmCard extends StatelessWidget {
                         height: 20,
                         width: 30,
                         child: Image.asset('assets/images/Slime.png')),
-                    Text(data['title'].toString(), style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: BLUE_BLACK_COLOR),),
+                    Text(type[data.type], style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: BLUE_BLACK_COLOR),),
                   ],
                 ),
                 Row(
                   children: [
-                    Text(data['time'].toString(), style: const TextStyle(fontSize: 14, color: DARK_GREY_COLOR ),),
-                    (delFlag)? TextButton( onPressed: () { }, child: Image.asset('assets/icon/delete.png',), ): const SizedBox.shrink()
+                    Text(data.sendtime, style: const TextStyle(fontSize: 14, color: DARK_GREY_COLOR ),),
+                    (delFlag)? TextButton( onPressed: () async {
+                      int? memberId = Provider.of<MemberProvider>(context, listen: false).member?.memberId;
+                      await Provider.of<AlarmProvider>(context, listen: false).deleteAlarm(data.notificationId);
+                      Provider.of<AlarmProvider>(context, listen: false).getAlarm(memberId!);
+                    }, child: Image.asset('assets/icon/delete.png',), ): const SizedBox.shrink()
                   ],
                 ),
               ],
             ),
             SizedBox(height: 4),
-            Text(data['message'].toString(), style: const TextStyle(fontSize: 13, color: BLUE_BLACK_COLOR
+            Text(data.message, style: const TextStyle(fontSize: 13, color: BLUE_BLACK_COLOR
             ),),
           ],
         ),
