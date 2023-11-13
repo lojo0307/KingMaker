5228e739294b8d832f022a42f22c272efa0afdd6
Seulgi <urina842@gmail.com>
2023-11-13 17:26:25 +0900

    :lipstick: style: 상태창 디자인 변경

--- frontend/kingmaker/lib/widget/main/main_expbar.dart
+++ frontend/kingmaker/lib/widget/main/main_expbar.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
 
 class ExpBar extends StatefulWidget {
   const ExpBar({super.key});
@@ -11,52 +13,79 @@ class ExpBar extends StatefulWidget {
 class _ExpBarState extends State<ExpBar> {
   @override
   Widget build(BuildContext context) {
+    //상태표시줄
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
+
     return LayoutBuilder(builder: (context, constraints) {
       final double maxWidth = constraints.maxWidth;
       return Container(
+        margin: EdgeInsets.fromLTRB(10, 60, 10, 0),
+        padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+
         width: maxWidth,
-        height: 72,
-        padding: EdgeInsets.only(top: 25),
+        height: 55,
+        // padding: EdgeInsets.only(top: 25),
+
         decoration: BoxDecoration(
-          color: Colors.white54, // 배경색을 하얀색으로 설정합니다.
+          color: Colors.white, // 배경색을 하얀색으로 설정합니다.
+          borderRadius: BorderRadius.all(Radius.circular(10)),
           // borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10),bottomRight: Radius.circular(10)),
         ),
         child:
-          Row(
-            children: [
-              Container(
-                padding: EdgeInsets.only(left: 10),
+        Row(
+          children: [
+            Container(
+              padding: EdgeInsets.only(left: 10),
 
-                width: 35,
-                height: 35,
+              width: 35,
+              height: 35,
 
-                child: Image.asset('assets/images/expbar/expicon.png', height: 10, width: 10,),
-              ),
-              Container(
-                width: maxWidth-150, // Container의 너비를 조절합니다.
-                height: 100,
+              child: Image.asset('assets/images/expbar/expicon.png', height: 10, width: 10,),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 15),
+              padding: EdgeInsets.only(left: 10),
 
-                child: Image.asset('assets/images/expbar/1_expbar.png', height: 70,),
-              ),
+              //bar
+              child: LinearPercentIndicator(
+                width: 220.0,
+                animation: true,
+                animationDuration: 100,
+                lineHeight: 50.0,
 
-              Container(
-                padding: EdgeInsets.only(left: 10),
+                //백성 수 연결하기!
+                //백성 수/100000 을 나타내기
+                percent: 50000/100000,
+                center: Text("50000명"),
 
-                width: 45,
-                height: 45,
-                // color: Colors.white70,
-                child: Image.asset('assets/images/expbar/monstericon.png', height: 10, width: 10,),
-              ),
-              Container(
-                alignment: Alignment.center,
-                padding: EdgeInsets.only(left: 10,top: 3),
-                height: 40,
-
-                child: Text('X 10', style: TextStyle(fontSize: 20),),
+                progressColor: Colors.green,
+                barRadius: Radius.circular(10),
               ),
 
-            ],
-          ),
+              //img
+              // width: maxWidth-150, // Container의 너비를 조절합니다.
+              // height: 100,
+              // child: Image.asset('assets/images/expbar/1_expbar.png', height: 70,),
+            ),
+
+            Container(
+              padding: EdgeInsets.only(left: 10),
+
+              width: 45,
+              height: 45,
+              // color: Colors.white70,
+              child: Image.asset('assets/images/expbar/monstericon.png', height: 10, width: 10,),
+            ),
+            Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.only(left: 10,top: 3),
+              height: 40,
+
+              child: Text('X 10', style: TextStyle(fontSize: 20),),
+            ),
+
+          ],
+        ),
 
 
       );
