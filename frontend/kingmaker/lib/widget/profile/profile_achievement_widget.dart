41cb439406d337ec2b5b94569467e9e0b26554f2
lojo0307 <lojo0307@naver.com>
2023-11-14 16:03:54 +0900

    :lipstick: Style: 마이페이지 스타일 수정

--- frontend/kingmaker/lib/widget/profile/profile_achievement_widget.dart
+++ frontend/kingmaker/lib/widget/profile/profile_achievement_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:kingmaker/consts/colors.dart';
 import 'package:kingmaker/dto/reward_dto.dart';
 import 'package:kingmaker/provider/achievement_provider.dart';
 import 'package:kingmaker/widget/achievement/achievement_page.dart';
@@ -10,7 +12,8 @@ class ProfileAchievementWidget extends StatefulWidget {
   const ProfileAchievementWidget({super.key});
 
   @override
-  State<ProfileAchievementWidget> createState() => _ProfileAchievementWidgetState();
+  State<ProfileAchievementWidget> createState() =>
+      _ProfileAchievementWidgetState();
 }
 
 class _ProfileAchievementWidgetState extends State<ProfileAchievementWidget> {
@@ -21,80 +24,55 @@ class _ProfileAchievementWidgetState extends State<ProfileAchievementWidget> {
       return Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Container(
-            width: constraints.maxWidth - 40,
-            padding: EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(13),
-              border: Border.all(
-                color: Colors.grey,
-                width: 1,
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(24),
+              decoration: BoxDecoration(
+                color: WHITE_COLOR,
+                borderRadius: BorderRadius.circular(20),
               ),
-            ),
-            child: Column(
-              children: [
-                Container(
-                  padding: EdgeInsets.only(bottom: 10),
-                  child:Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text('달성한 업적',
-                        style: TextStyle(
-                          fontSize: 18,
-                          // fontWeight: FontWeight.bold,
+              child: Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.zero,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          '달성한 업적',
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
                         ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          TextButton(
-                            style: ButtonStyle(
-                              // backgroundColor: Colors.blue
-                            ),
-                            onPressed: () {
-                              Navigator.push(
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
                                 context,
                                 MaterialPageRoute(
-                                    builder: (context) => AchievementPage())
-                                );
-                              },
-                            child: const Text('더보기', style: TextStyle(fontSize: 12),),
+                                    builder: (context) => AchievementPage()));
+                          },
+                          child: Row(
+                            children: [
+                              Text(
+                                '더보기',
+                                style: TextStyle(fontSize: 12, color: BLUE_BLACK_COLOR),
+                              ),
+                              SvgPicture.asset('assets/icon/ic_right.svg')
+                            ],
                           ),
-                          Container(
-                            width: 26,
-                            height: 26,
-                            // decoration: BoxDecoration(color: Colors.red),
-                            child: IconButton(
-                              // color: Colors.red,
-                              icon: Image.asset('assets/icon/tight_right.png',
-                                scale: 1.4,),
-                              // padding: EdgeInsets.only(left: -10),
-                              onPressed: () {
-                                print("click");
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => AchievementPage())
-                                );
-                              },
-                              iconSize: 10,
-                            ),
-                          )
-                        ],
-                      )
-
-                    ],
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-
-                //이 부분에 위젯이 들어가야해
-                Wrap(
-                  spacing: 11.0, // 각 아이콘 사이의 가로 간격
-                  runSpacing: 7.0, // 각 아이콘 줄 사이의 세로 간격
-                  children: getMyRewardThree(list),
-                ),
-              ],
+                  SizedBox(height: 16,),
+                  //이 부분에 위젯이 들어가야해
+                  Wrap(
+                    spacing: 11.0, // 각 아이콘 사이의 가로 간격
+                    runSpacing: 7.0, // 각 아이콘 줄 사이의 세로 간격
+                    children: getMyRewardThree(list),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
@@ -107,14 +85,14 @@ class _ProfileAchievementWidgetState extends State<ProfileAchievementWidget> {
     if (list.isEmpty) {
       return [Container()];
     }
-    for(int i = 0 ; i < 3 ; i++){
+    for (int i = 0; i < 3; i++) {
       RewardDto now = list.elementAt(i);
       if (now.achieved) {
         resList.add(ProfileAchievementIconWidget(data: now));
       }
     }
     if (resList.isEmpty) {
-      return[Container()];
+      return [Container()];
     }
     return resList;
   }
--- frontend/kingmaker/lib/widget/profile/profile_score_widget.dart
+++ frontend/kingmaker/lib/widget/profile/profile_score_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kingmaker/consts/colors.dart';
 import 'package:kingmaker/provider/achievement_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:simple_animation_progress_bar/simple_animation_progress_bar.dart';
@@ -22,130 +23,126 @@ class _ProfileScoreWidgetState extends State<ProfileScoreWidget> {
       return Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Container(
-            width: constraints.maxWidth - 40,
-            padding: EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(13),
-              border: Border.all(
-                color: Colors.grey,
-                width: 1,
+          Expanded(
+            child: Container(
+              width: constraints.maxWidth - 40,
+              padding: EdgeInsets.all(24),
+              decoration: BoxDecoration(
+                color: WHITE_COLOR,
+                borderRadius: BorderRadius.circular(20),
               ),
-            ),
-            child: Column(
-              children: [
-                Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text('달성률',
-                        style: TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
+              child: Column(
+                children: [
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        const Text('몬스터 리포트',
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  child: Column(
-                    children: [
-                      Row(children: [
-                        Text("일간: "),
-                        SimpleAnimationProgressBar(
-                          height: 10,
-                          width: constraints.maxWidth*0.6,
-                          backgroundColor: Colors.grey.shade800,
-                          foregrondColor: Colors.purple,
-                          ratio: 0.01*dailyRate,
-                          direction: Axis.horizontal,
-                          curve: Curves.fastLinearToSlowEaseIn,
-                          duration: const Duration(seconds: 3),
-                          borderRadius: BorderRadius.circular(10),
-                          gradientColor: const LinearGradient(
-                              colors: [Colors.pink, Colors.purple]),
-                        ),
-                        Text('  $dailyRate%'),
-                      ],),
-                      Row(children: [
-                        Text("월간: "),
-                        SimpleAnimationProgressBar(
-                          height: 10,
-                          width: constraints.maxWidth*0.6,
-                          backgroundColor: Colors.grey.shade800,
-                          foregrondColor: Colors.teal,
-                          ratio: 0.01*monthlyRate,
-                          direction: Axis.horizontal,
-                          curve: Curves.fastLinearToSlowEaseIn,
-                          duration: const Duration(seconds: 3),
-                          borderRadius: BorderRadius.circular(10),
-                          gradientColor: const LinearGradient(
-                              colors: [Colors.tealAccent, Colors.teal]),
-                        ),
-                        Text('  $monthlyRate%'),
                       ],
-                      ),
-                      Row(children: [
-                        Text("연간: "),
-                        SimpleAnimationProgressBar(
-                          height: 10,
-                          width: constraints.maxWidth*0.6,
-                          backgroundColor: Colors.grey.shade800,
-                          foregrondColor: Colors.indigo,
-                          ratio: 0.01*yearRate,
-                          direction: Axis.horizontal,
-                          curve: Curves.fastLinearToSlowEaseIn,
-                          duration: const Duration(seconds: 3),
-                          borderRadius: BorderRadius.circular(10),
-                          gradientColor: const LinearGradient(
-                              colors: [Colors.indigoAccent, Colors.indigo]),
-                        ),
-                        Text('  $yearRate%'),
-                      ],),
-                    ],
+                    ),
                   ),
-                ),
-                Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text('가장 강력한 몬스터',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),),
-                      Text(maxCategory,
-                        style: const TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.red,
+                  SizedBox(height: 8,),
+                  Container(
+                    child: Column(
+                      children: [
+                        Row(children: [
+                          Text("일간  ", style: TextStyle(fontSize: 14                                                                                    ),),
+                          SimpleAnimationProgressBar(
+                            height: 10,
+                            width: constraints.maxWidth*0.6,
+                            backgroundColor: GREY_COLOR,
+                            foregrondColor: BLUE_COLOR,
+                            ratio: 0.01*dailyRate,
+                            direction: Axis.horizontal,
+                            curve: Curves.fastLinearToSlowEaseIn,
+                            duration: const Duration(seconds: 3),
+                            borderRadius: BorderRadius.circular(10),
+                            gradientColor: const LinearGradient(
+                                colors: [LIGHT_BLUE_COLOR, BLUE_COLOR]),
+                          ),
+                          Text('  $dailyRate%', style: TextStyle(fontSize: 12),),
+                        ],),
+                        Row(children: [
+                          Text("월간  ", style: TextStyle(fontSize: 14),),
+                          SimpleAnimationProgressBar(
+                            height: 10,
+                            width: constraints.maxWidth*0.6,
+                            backgroundColor: GREY_COLOR,
+                            foregrondColor: DARK_BLUE_COLOR,
+                            ratio: 0.01*monthlyRate,
+                            direction: Axis.horizontal,
+                            curve: Curves.fastLinearToSlowEaseIn,
+                            duration: const Duration(seconds: 3),
+                            borderRadius: BorderRadius.circular(10),
+                            gradientColor: const LinearGradient(
+                                colors: [BLUE_COLOR, DARK_BLUE_COLOR]),
+                          ),
+                          Text('  $monthlyRate%', style: TextStyle(fontSize: 12),),
+                        ],
                         ),
-                      ),
-                    ],
+                        Row(children: [
+                          Text("연간  ", style: TextStyle(fontSize: 14),),
+                          SimpleAnimationProgressBar(
+                            height: 10,
+                            width: constraints.maxWidth*0.6,
+                            backgroundColor: GREY_COLOR,
+                            foregrondColor: DARKER_BLUE_COLOR,
+                            ratio: 0.01*yearRate,
+                            direction: Axis.horizontal,
+                            curve: Curves.fastLinearToSlowEaseIn,
+                            duration: const Duration(seconds: 3),
+                            borderRadius: BorderRadius.circular(10),
+                            gradientColor: const LinearGradient(
+                                colors: [DARK_BLUE_COLOR, DARKER_BLUE_COLOR]),
+                          ),
+                          Text('  $yearRate%', style: TextStyle(fontSize: 12),),
+                        ],),
+                      ],
+                    ),
                   ),
-                ),
-                Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text('가장 약한 몬스터',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text('가장 강력한 몬스터',
+                          style: TextStyle(
+                            fontSize: 14,
+                          ),),
+                        Text(maxCategory,
+                          style: const TextStyle(
+                            fontSize: 14,
+                            fontFamily: 'EsamanruMedium',
+                            color: Color(0xFFFA5959),
+                          ),
                         ),
-                      ),
-                      Text(minCategory,
-                        style: const TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.green,
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text('가장 약한 몬스터',
+                          style: TextStyle(
+                            fontSize: 14,
+                          ),
+                        ),
+                        Text(minCategory,
+                          style: const TextStyle(
+                            fontSize: 14,
+                            fontFamily: 'EsamanruMedium',
+                            color: Color(0xFF54B08F),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           )],
       );
