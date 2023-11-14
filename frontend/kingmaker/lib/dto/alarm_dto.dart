54d1698f4a195222f6ab7a2d13f0943f0515dfe2
허성백 <heosb123@hanmail.net>
2023-11-14 14:55:31 +0900

    feat: 알림 페이지 디자인 추가

--- frontend/kingmaker/lib/dto/alarm_dto.dart
+++ frontend/kingmaker/lib/dto/alarm_dto.dart
@@ -3,22 +3,20 @@ class AlarmDto{
   String message;
   int notificationId;
   String sendtime;
-  int type;
 
   AlarmDto({
     required this.notificationTypeId,
     required this.message,
     required this.notificationId,
     required this.sendtime,
-    required this.type,
   });
   factory AlarmDto.fromJson(Map<String, dynamic> json) {
+    print("dkdkdkdkdkdkkd아멀;ㄴ이ㅏ럼;ㄴ이ㅏ : ${json}");
     return AlarmDto(
       notificationTypeId: json['notificationTypeId'] ?? 0,
       message: json['message'] ?? "",
       notificationId: json['notificationId'] ?? 0,
       sendtime: json['sendtime'] ?? "",
-      type: json['type'] ?? 1,
     );
   }
 }
