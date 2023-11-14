f3b4b51253a424dde0eef37a8523ce3dd3992dc9
김병찬 <qudcks8749@naver.com>
2023-11-14 11:13:21 +0900

    Merge: bgm update

---
+++ frontend/kingmaker/lib/widget/bgm/audioplayer_helper.dart
@@ -0,0 +1,34 @@
+import 'package:assets_audio_player/assets_audio_player.dart';
+
+class AudioPlayerHelper {
+  static final AudioPlayerHelper _instance = AudioPlayerHelper._internal();
+  final AssetsAudioPlayer _assetsAudioPlayer = AssetsAudioPlayer.newPlayer();
+
+  factory AudioPlayerHelper() {
+    return _instance;
+  }
+
+  AudioPlayerHelper._internal();
+
+  void playLoginMusic(String title) {
+    _assetsAudioPlayer.open(
+      Audio("assets/audios/$title"),
+      loopMode: LoopMode.single,
+      autoStart: true,
+      showNotification: false,
+      volume: 0.5, // 볼륨 설정 (0.0 무음 ~ 1.0 최대 볼륨)
+    );
+  }
+
+  void setVolume(double volume) {
+    _assetsAudioPlayer.setVolume(volume);
+  }
+
+  void stopMusic() {
+    _assetsAudioPlayer.stop();
+  }
+
+  void dispose() {
+    _assetsAudioPlayer.dispose();
+  }
+}
