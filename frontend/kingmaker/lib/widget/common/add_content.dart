3569ae2a96daf31584cc87a486cbb40f6df0695e
lojo0307 <lojo0307@naver.com>
2023-11-14 13:41:49 +0900

    :lipstick: Style: 일정 플러스버튼 스타일 수정

--- frontend/kingmaker/lib/widget/common/add_content.dart
+++ frontend/kingmaker/lib/widget/common/add_content.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:kingmaker/consts/colors.dart';
 import 'package:kingmaker/widget/routine/regist_routine.dart';
 import 'package:kingmaker/widget/todo/regist_todo.dart';
 
@@ -22,7 +24,7 @@ class _AddContentState extends State<AddContent> {
               setState(() {});
             },
             child: Scaffold(
-              backgroundColor: Colors.black.withOpacity(0.5),
+              backgroundColor: Colors.black.withOpacity(0.2),
               body: addPage(),
             ),
           )
@@ -44,23 +46,36 @@ class _AddContentState extends State<AddContent> {
                       margin: const EdgeInsetsDirectional.symmetric(
                         horizontal: 10,
                       ),
-                      child: TextButton(
-                        style: const ButtonStyle(
-                          backgroundColor:
-                              MaterialStatePropertyAll(Colors.white),
-                        ),
-                        onPressed: () {
-                          flag = !flag;
-                          setState(() {});
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => RegistTodo()));
-                        },
-                        child: const Text(
-                          '할일 등록하기',
-                          style: TextStyle(color: Colors.black),
-                        ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          SizedBox(),
+                          TextButton(
+                            style: const ButtonStyle(
+                              backgroundColor:
+                                  MaterialStatePropertyAll(WHITE_COLOR),
+                            ),
+                            onPressed: () {
+                              flag = !flag;
+                              setState(() {});
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => RegistTodo()));
+                            },
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                SvgPicture.asset('assets/icon/ic_todo_regist.svg'),
+                                SizedBox(width: 4.0,),
+                                Text(
+                                  '할일 등록하기',
+                                  style: TextStyle(color: BLUE_BLACK_COLOR, fontSize: 12),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
                     )
                   : const SizedBox.shrink(),
@@ -69,45 +84,72 @@ class _AddContentState extends State<AddContent> {
                       margin: const EdgeInsetsDirectional.symmetric(
                         horizontal: 10,
                       ),
-                      child: TextButton(
-                        style: const ButtonStyle(
-                          backgroundColor:
-                              MaterialStatePropertyAll(Colors.white),
-                        ),
-                        onPressed: () {
-                          flag = !flag;
-                          setState(() {});
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => RegistRoutine()));
-                        },
-                        child: const Text(
-                          '루틴 등록하기',
-                          style: TextStyle(color: Colors.black),
-                        ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          SizedBox(width: 1,),
+                          TextButton(
+                            style: const ButtonStyle(
+                              backgroundColor:
+                                  MaterialStatePropertyAll(WHITE_COLOR),
+                            ),
+                            onPressed: () {
+                              flag = !flag;
+                              setState(() {});
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => RegistRoutine()));
+                            },
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                SvgPicture.asset('assets/icon/ic_routine.svg'),
+                                SizedBox(width: 4,),
+                                Text(
+                                  '루틴 등록하기',
+                                  style: TextStyle(color: BLUE_BLACK_COLOR, fontSize: 12),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
                     )
                   : const SizedBox.shrink(),
-              TextButton(
-                onPressed: () {
-                  flag = !flag;
-                  setState(() {});
-                },
-                child: Container(
-                  width: 70,
-                  height: 70,
-                  decoration: BoxDecoration(
-                      color: const Color(0xFF9FC6D2),
-                      borderRadius: BorderRadius.circular(100),
-                      border: Border.all(color: Colors.white)),
-                  child: const Icon(
-                    CupertinoIcons.plus,
-                    color: Colors.white,
-                    size: 70,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      flag = !flag;
+                      setState(() {});
+                    },
+                    child: Container(
+                      width: 56,
+                      height: 56,
+                      decoration: BoxDecoration(
+                          color: BLUE_COLOR,
+                          borderRadius: BorderRadius.circular(100),
+                        boxShadow: [
+                          BoxShadow(
+                              color: BLUE_BLACK_COLOR.withOpacity(0.15), // 그림자 색상
+                            spreadRadius: 2, // 퍼짐 정도
+                            blurRadius: 8, // 흐림 정도
+                            offset: Offset(2, 5), // 그림자의 위치 조정 (가로, 세로)
+                          ),
+                        ],),
+                      child: const Icon(
+                        CupertinoIcons.plus,
+                        color: WHITE_COLOR,
+                        size: 40,
+                      ),
+                    ),
                   ),
-                ),
+                  SizedBox(width: 4,)
+                ],
               ),
+              SizedBox(height: 4,)
             ],
           ),
         )
