6cb855d17d4fb95ee2b7f49d72746a86e5a7bb1c
lojo0307 <lojo0307@naver.com>
2023-11-14 16:03:54 +0900

    :lipstick: Style: 마이페이지 스타일 수정

--- frontend/kingmaker/lib/widget/profile/profile_kingdom_widget.dart
+++ frontend/kingmaker/lib/widget/profile/profile_kingdom_widget.dart
@@ -26,78 +26,70 @@ class _ProfileKingdomWidgetState extends State<ProfileKingdomWidget> {
     int mem = citizen < 0 ? -citizen : citizen;
     return LayoutBuilder(builder: (ctx, constraints) {
       return Row(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Container(
-            width: constraints.maxWidth - 40,
-            padding: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(13),
-              border: Border.all(
-                color: Colors.grey,
-                width: 1,
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
               ),
-            ),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  child: Image(image: AssetImage('assets/images/castle/Lv${kingdom?.level}.png',),
-                    height: 100,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    child: Image(image: AssetImage('assets/images/castle/Lv${kingdom?.level}.png',),
+                      height: 100,
+                    ),
                   ),
-                ),
-                SizedBox(
-                  width: constraints.maxWidth - 190,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(kingdom!.kingdomNm,
-                        style: const TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
+                  SizedBox(
+                    width: constraints.maxWidth - 190,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(kingdom!.kingdomNm,
+                          style: const TextStyle(
+                            fontSize: 18,
+                          ),
                         ),
-                      ),
-                      const SizedBox(height: 5,),
-                      Text('왕국 ${kingdom?.level} 단계',
-                        style: const TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
+                        const SizedBox(height: 8,),
+                        Text('왕국 ${kingdom?.level} 단계',
+                          style: const TextStyle(
+                            fontSize: 12,
+                          ),
                         ),
-                      ),
-                      const SizedBox(height: 5,),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text('백성 : ${kingdom?.citizen} 명',
-                            style: const TextStyle(
-                              fontSize: 15,
-                              fontWeight: FontWeight.bold,
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('백성 : ${kingdom?.citizen} 명',
+                              style: const TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          ),
-                          Text(' (',
-                            style: TextStyle(
-                              color: citizen > 0 ? Colors.red : citizen == 0 ? Colors.black : Colors.blue,
-                              fontSize: 15,
-                              fontWeight: FontWeight.bold,
+                            Text(' (',
+                              style: TextStyle(
+                                color: citizen > 0 ? Colors.red : citizen == 0 ? Colors.black : Colors.blue,
+                                fontSize: 12,
+                              ),
                             ),
-                          ),
-                          getIcone(citizen),
-                          Text('${mem == 0 ? "" : mem} )',
-                            style: TextStyle(
-                              color: citizen > 0 ? Colors.red : citizen == 0 ? Colors.black : Colors.blue,
-                              fontSize: 15,
-                              fontWeight: FontWeight.bold,
+                            getIcone(citizen),
+                            Text('${mem == 0 ? "" : mem} )',
+                              style: TextStyle(
+                                color: citizen > 0 ? Colors.red : citizen == 0 ? Colors.black : Colors.blue,
+                                fontSize: 12,
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                    ],
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           )],
       );
