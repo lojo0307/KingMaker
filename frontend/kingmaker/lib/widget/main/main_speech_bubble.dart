291c7479a851703be223af832d444d93d71a72c1
허성백 <heosb123@hanmail.net>
2023-11-15 16:08:55 +0900

    :sparkles: feat: 왕 눌렀을 때 말풍선 대사 변경

--- frontend/kingmaker/lib/widget/main/main_speech_bubble.dart
+++ frontend/kingmaker/lib/widget/main/main_speech_bubble.dart
@@ -8,7 +8,12 @@ class MainSpeechBubble extends PositionComponent with HasGameRef {
   late SpriteComponent bubbleSprite;
   late TextComponent textComponent;
   MainPlayer? mainPlayer;
-  List<String> list = ['곤방와','대단해~!','우효-',' 왕으로 \n 만들어줘'];
+  List<String> list = ['이리 오너라', '여기는 어디?', '으악!\n고블린이다!',
+    '내가 왕...?', '허성백\n최고', '백성들은\n들으라', '스프가 짜다', '동맹할 사람?',
+  '반란은\n용서모태!', '노 세금\n노 복지', '국경을\n강화하라!', '평화는\n무력으로!',
+  '백투더 퓨처?', '젠킨스?', '문을 열어라!', '예를 갖춰\n대하라', '날씨가\n좋구나',
+  '오늘 점심이\n무엇이냐?', '귀찮구나', '밥 줘', '짐의 나이가\n궁금한가?', '혁명을\n일으키겠다',
+  '왕보다 낮은\n사람 접어', '내가 왕이될\n상인가', '부모도\n실력이다'];
   final text;
   MainSpeechBubble(Vector2 position, String this.text)
       : super(position: position, size: Vector2(130, 122));
@@ -24,7 +29,7 @@ class MainSpeechBubble extends PositionComponent with HasGameRef {
       textRenderer: TextPaint(
         style: TextStyle(
           fontFamily: 'PretendardBold',
-          fontSize: 20,
+          fontSize: 18,
           color: Colors.black,
         ),
       ),
