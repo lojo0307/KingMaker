ab3dafa9ec03e67a5a2d1ada5859c71cc79d6cdc
Seulgi <urina842@gmail.com>
2023-11-16 10:54:39 +0900

    :pencil2: fix: 알림 시 sendTime 보내는 걸로 변경

--- backend/src/main/java/com/dollyanddot/kingmaker/domain/notification/dto/response/NotificationResDto.java
+++ backend/src/main/java/com/dollyanddot/kingmaker/domain/notification/dto/response/NotificationResDto.java
@@ -1,6 +1,7 @@
 package com.dollyanddot.kingmaker.domain.notification.dto.response;
 
 import com.dollyanddot.kingmaker.domain.notification.domain.Notification;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,11 +15,14 @@ public class NotificationResDto {
     String message;
     int notificationTypeId;
     Long notificationId;
+    LocalDateTime sendTime;
+
     public static NotificationResDto toDto(Notification entity) {
         return NotificationResDto.builder()
                 .message(entity.getMessage())
                 .notificationTypeId(entity.getNotificationType().getNotificationTypeId())
                 .notificationId(entity.getNotificationId())
+                .sendTime(entity.getCreatedAt())
                 .build();
     }
 
