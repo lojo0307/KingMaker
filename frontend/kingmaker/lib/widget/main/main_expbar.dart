74994949ec96df7371768658e433e9a92902315b
KimByeongChan <qudcks8749@naver.com>
2023-11-16 10:07:14 +0900

    :zap: feat: 경험치 바  크기에 맞게 수정

--- frontend/kingmaker/lib/widget/main/main_expbar.dart
+++ frontend/kingmaker/lib/widget/main/main_expbar.dart
@@ -1,91 +1,79 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
 import 'package:kingmaker/dto/kingdom_dto.dart';
 import 'package:kingmaker/provider/kingdom_provider.dart';
 import 'package:kingmaker/provider/schedule_provider.dart';
 import 'package:percent_indicator/linear_percent_indicator.dart';
-import 'package:provider/provider.dart';
 
-class ExpBar extends StatefulWidget {
+class ExpBar extends StatelessWidget {
   const ExpBar({super.key});
 
   @override
-  State<ExpBar> createState() => _ExpBarState();
-}
-
-class _ExpBarState extends State<ExpBar> {
-  @override
   Widget build(BuildContext context) {
-    KingdomDto? kingdomDto = context.watch<KingdomProvider>().kingdomDto;
-    int leftMonster = context.watch<ScheduleProvider>().leftMonster;
-    return Container(
-        margin: EdgeInsets.fromLTRB(10, 55, 10, 0),
-        padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-        height: 55,
-        // padding: EdgeInsets.only(top: 25),
+    final kingdomDto = context.watch<KingdomProvider>().kingdomDto;
+    final int leftMonster = context.watch<ScheduleProvider>().leftMonster;
 
-        decoration: BoxDecoration(
-          color: Colors.white, // 배경색을 하얀색으로 설정합니다.
-          borderRadius: BorderRadius.all(Radius.circular(10)),
-          // borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10),bottomRight: Radius.circular(10)),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+      child: Container(
+        margin: EdgeInsetsDirectional.only(top: 13),
+        decoration: _boxDecoration(),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: <Widget>[
+              _expIcon(),
+              SizedBox(width: 7),
+              _expBar(kingdomDto, context),
+              SizedBox(width: 7),
+              _monsterIcon(),
+              _monsterCount(leftMonster),
+            ],
+          ),
         ),
-        child:
-        Row(
-          children: [
-            Container(
-              padding: EdgeInsets.only(left: 10),
-
-              width: 35,
-              height: 35,
-
-              child: Image.asset('assets/images/expbar/expicon.png', height: 10, width: 10,),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 15),
-              padding: EdgeInsets.only(left: 10),
-
-              //bar
-              child: LinearPercentIndicator(
-                width: 220.0,
-                animation: true,
-                animationDuration: 100,
-                lineHeight: 50.0,
-
-                //백성 수 연결하기!
-                //백성 수/100000 을 나타내기
-                percent: kingdomDto!.citizen/100000 > 1.0 ? 1.0 : kingdomDto!.citizen/100000,
-                center: Text("${kingdomDto?.citizen}명"),
-
-                progressColor: Colors.green,
-                barRadius: Radius.circular(10),
-              ),
-
-              //img
-              // width: maxWidth-150, // Container의 너비를 조절합니다.
-              // height: 100,
-              // child: Image.asset('assets/images/expbar/1_expbar.png', height: 70,),
-            ),
-
-            Container(
-              padding: EdgeInsets.only(left: 10),
-
-              width: 45,
-              height: 45,
-              // color: Colors.white70,
-              child: Image.asset('assets/images/expbar/monstericon.png', height: 10, width: 10,),
-            ),
-            Container(
-              alignment: Alignment.center,
-              padding: EdgeInsets.only(left: 10,top: 3),
-              height: 40,
-
-              child: Text('X $leftMonster', style: TextStyle(fontSize: 20),),
-            ),
-
-          ],
-        ),
-
-
+      ),
     );
+  }
+
+  BoxDecoration _boxDecoration() {
+    return BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(10),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.grey.withOpacity(0.5),
+          spreadRadius: 1,
+          blurRadius: 3,
+          offset: Offset(0, 3),
+        ),
+      ],
+    );
+  }
+
+  Widget _expIcon() {
+    return Image.asset('assets/images/expbar/expicon.png', width: 27, height: 27);
+  }
+
+  Widget _expBar(KingdomDto? kingdomDto, BuildContext context) {
+    final double percent = (kingdomDto?.citizen ?? 0) / 100000;
+    return Expanded(
+      child: LinearPercentIndicator(
+        animation: true,
+        lineHeight: 30.0,
+        percent: percent > 1.0 ? 1.0 : percent,
+        center: Text("${kingdomDto?.citizen ?? 0}명"),
+        progressColor: Colors.green,
+        barRadius: const Radius.circular(10),
+      ),
+    );
+  }
+
+  Widget _monsterIcon() {
+    return Image.asset('assets/images/expbar/monstericon.png', width: 33, height: 33);
+  }
+
+  Widget _monsterCount(int leftMonster) {
+    return Text('X $leftMonster', style: TextStyle(fontSize: 15));
   }
 }
